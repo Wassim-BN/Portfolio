81e35ac9d1acda1960e21c8d146fb54c5138620a
Wassim-BN <wassimo294@gmail.com>
2026-08-20 14:56:27 +0200

    navbar component

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+<template>
+  <header role="banner" class="flex sticky px-4 py-2 border">
+    <h1 class="">BEN NACEUR Wassim</h1>
+    <nav role="navigation">
+    </nav>
+    <div role="search">
+      <form action="" method="POST">
+      </form>
+    </div>
+  </header>
+</template>
